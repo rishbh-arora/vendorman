ac95bf7192a78fbabe091fab315900d0e4c3b5e0
Rishbh Arora <vsryarora6879@gmail.com>
2024-05-07 00:29:47 +0530

    route user model, fix history routing

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
 from .views import *
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import DefaultRouter, SimpleRouter
 from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r'purchase_orders', PurchaseViewset)
 router.register(r'vendors', VendorViewset)
-router.register(r'vendors', HistoryViewset)
+router.register(r'history', HistoryViewset)
 
 urlpatterns = router.urls
 
 urlpatterns += [
     path("token", obtain_auth_token),
+    path("users", UserViewset.as_view()),
+    path("users/<username>", UserViewset.as_view())
 ]
