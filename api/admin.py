58f9efce124b6e207644953df5780a78b0747054
Rishbh Arora <vsryarora6879@gmail.com>
2024-05-07 00:26:33 +0530

    Register models on admin page

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
+admin.site.register(PurchaseOrder)
+admin.site.register(History)
+admin.site.register(Vendor)
 # Register your models here.
